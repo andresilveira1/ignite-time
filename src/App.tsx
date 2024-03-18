776bf4cdc38cc8732275721c304c1d3fe9557b7f
André Silveira <andre.silveira1silva@gmail.com>
2024-03-18 15:41:15 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStype } from './styles/global'
+
+import { Router } from './Router'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStype />
+    </ThemeProvider>
+  )
+}
---
+++ src/pages/History.tsx
@@ -0,0 +1,3 @@
+export function History() {
+  return <h1>Hello World 2</h1>
+}
